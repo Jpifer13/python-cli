7c67fbecaceb8dbb1e5ae4682f575b8caaadc015
Jake <jakepifer3@gmail.com>
2019-09-09 23:20:07 -0400

    added individual and bulk update options

--- pythonCLI.py
+++ pythonCLI.py
@@ -7,36 +7,44 @@ import subprocess
 from functions import *
 from values import *
 
-
 @click.group()
 @click.option('--ip', '-i', help='This is the ip address of the device you wish to connect to.')
+@click.option('--new_ip', '-ni', help='This is the ip address of the new device.', default="")
 @click.option('--password', '-p', help='The password of the device you are trying to connect to.',)
 @click.option('--user', '-u', help='The username of the device you are trying to connect to.',)
 @click.option('--command', '-c', help='The command you wish to run on each device.',)
 @click.option('--syspass', '-sp', help='The new system password')
 @click.option('--sysuser', '-su', help='The new system username')
-@click.option('--bulk', '-b', help='States whether this is a bulk action.')
+@click.option('--bulk', '-b', help='States whether this is a bulk action.', type=bool, default=False)
 @click.pass_context
-def cli(ctx, ip, user, password, command, sysuser, syspass, bulk):
+def cli(ctx, ip, user, password, command, sysuser, syspass, bulk, new_ip):
     """
     CLI tool used for SSH into rpi 4's and using commands
     """
-
-    if(ip):
-        ctx.obj['IP'] = ip
+    print(command)
+    print(bulk)
+    ctx.obj['BULK'] = bulk
+    if(new_ip):
         newDevice = {
             'USER': user,
             'PASSWORD': password
         }
         ctx.obj[str(ip)] = newDevice
-    elif(command):
+    if(ip):
+        if(ctx.obj[ip] == None):
+            newDevice = {
+            'USER': user,
+            'PASSWORD': password
+            }
+            ctx.obj[str(ip)] = newDevice
+        else:
+            ctx.obj['IP'] = ip
+    if(command):
         ctx.obj['COMMAND'] = command
-    elif(sysuser):
+    if(sysuser):
         ctx.obj['SYSUSER'] = sysuser
-    elif(syspass):
+    if(syspass):
         ctx.obj['SYSPASS'] = syspass
-    elif(bulk):
-        ctx.obj['BULK'] = bulk
 
 
 @cli.command()
@@ -45,16 +53,18 @@ def testconn(ctx):
     """
     Test connection to rpi at given ip address. This will currently print out the temperature that 
     """
-
     # This loops through the dict of devices and connects and shows the temp for each one
     for key in ctx.obj:
-        ssh = connect(str(key), ctx.obj[str(key)]
-                      ['USER'], ctx.obj[str(key)]['PASSWORD'])
+        try:
+            ssh = connect(str(key), ctx.obj[str(key)]
+                        ['USER'], ctx.obj[str(key)]['PASSWORD'])
 
-        stdin, stdout, stderr = ssh.exec_command(
-            '/opt/vc/bin/vcgencmd measure_temp')
+            stdout = ssh.exec_command(
+                'date')
 
-        print(*stdout.readlines(), sep='\n')
+            print(*stdout.readlines(), sep='\n')
+        except:
+            pass
 
 
 @cli.command()
@@ -63,12 +73,12 @@ def cmd(ctx):
     """
     This will loop through selected devices, bulk or individual, and do the command given to it.
     """
-    print(ctx.obj['BULK'])
+    print(ctx.obj)
     # Check if bulk tag is absent or not
     if(ctx.obj['BULK']):
         # This loops through the dict of devices and runs the inputted command for each one
         for key in ctx.obj:
-            if(str(key) != 'COMMAND'):  # Check to make sure that the current iteration is an IP
+            try:  # Check to make sure that the current iteration is an IP
                 ssh = connect(str(key), ctx.obj[str(key)]
                               ['USER'], ctx.obj[str(key)]['PASSWORD'])
 
@@ -76,17 +86,19 @@ def cmd(ctx):
                     str(ctx.obj['COMMAND']))
 
                 print(*stdout.readlines(), sep='\n')
+            except:
+                pass
     # Bulk tab is absent
     else:
         # Check to make sure that IP address is present
-        if(ctx.obj['IP']):
-            ssh = connect(ctx.obj['IP'], ctx.obj['IP']
-                          ['USER'], ctx.obj['IP']['PASSWORD'])
+        try:
+            ssh = connect(ctx.obj['IP'], ctx.obj[ctx.obj['IP']]
+                          ['USER'], ctx.obj[ctx.obj['IP']]['PASSWORD'])
 
             stdin, stdout, stderr = ssh.exec_command(str(ctx.obj['COMMAND']))
 
             print(*stdout.readlines(), sep='\n')
-        else: # IP address and bulk tag are not present so state so
+        except: # IP address and bulk tag are not present so state so
             print("No IP address given and one is needed when not using bulk tag.")
 
 
@@ -107,6 +119,14 @@ def cmd(ctx):
 if __name__ == '__main__':
     cli(obj={
         '192.168.1.12': {
+            'USER': user,
+            'PASSWORD': password
+        },
+        '192.168.1.2': {
+            'USER': user,
+            'PASSWORD': password
+        },
+        '192.168.1.6': {
             'USER': user,
             'PASSWORD': password
         }
